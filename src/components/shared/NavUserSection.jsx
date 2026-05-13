9fddf54c5f3599d8f897dd732c3c49276e7865c1
piasmajumdar <piasmajumdar@gmail.com>
2026-05-13 22:29:31 +0530

    Made responsive profile photo with dropdown logout

--- src/components/shared/NavUserSection.jsx
+++ src/components/shared/NavUserSection.jsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { authClient } from "@/lib/auth-client";
+import Image from "next/image";
 import Link from "next/link";
 import { FaUser } from "react-icons/fa";
 
@@ -20,19 +21,22 @@ const NavUserSection = () => {
                             <div className="dropdown dropdown-end">
                                 <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                                     <div className="w-10 rounded-full">
-                                        <img
-                                            alt="Tailwind CSS Navbar component"
-                                            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                                        <Image
+                                            src={session?.user?.image}
+                                            alt={session?.user?.name}
+                                            width={40}
+                                            height={40}
+                                        />
                                     </div>
                                 </div>
                                 <ul
                                     tabIndex="-1"
                                     className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                                     <li>
-                                        <a className="justify-between">
+                                        <Link href={'/profile'} className="justify-between">
                                             Profile
                                             <span className="badge">New</span>
-                                        </a>
+                                        </Link>
                                     </li>
                                     <li><button onClick={async () => await authClient.signOut()}>Logout</button></li>
                                 </ul>
