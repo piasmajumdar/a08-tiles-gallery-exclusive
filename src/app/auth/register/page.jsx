afb05912f34a0dc065273774a10ad120dfa83a91
piasmajumdar <piasmajumdar@gmail.com>
2026-05-13 20:08:11 +0530

    better auth setup

--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -5,7 +5,12 @@ const SignUpPage = () => {
     return (
         <div className='h-screen flex justify-center items-center mx-auto'>
             <div className='bg-white rounded-lg shadow-2xl p-6 sm:w-sm space-y-2'>
-                <h2 className='text-3xl text-center'>Please Login!</h2>
+                <h2 className='text-3xl text-center'>Please Sign Up!</h2>
+                {/* Google */}
+                <button className="btn bg-white text-black my-5 w-full border-[#e5e5e5]">
+                    <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
+                    Sign Up with Google
+                </button>
                 <fieldset className="fieldset">
                     <label className="label">Name</label>
                     <input type="text" className="input w-full" placeholder="Name" />
@@ -18,7 +23,7 @@ const SignUpPage = () => {
                     <label className="label">Password</label>
                     <input type="password" className="input w-full" placeholder="Password" />
                 </fieldset>
-                <button className="btn btn-neutral mt-4 w-full">Login</button>
+                <button className="btn btn-neutral mt-4 w-full">Sign Up</button>
                 <h2>Already have an account? <Link href={'/auth/login'} className='text-blue-800'>Login</Link></h2>
             </div>
         </div>
