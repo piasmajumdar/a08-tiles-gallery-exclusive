bf35b66fffa547d53772a9d84906c9202c9fbb5a
piasmajumdar <piasmajumdar@gmail.com>
2026-05-13 21:27:20 +0530

    Google Login and Signup completed

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -21,7 +21,21 @@ const LoginPage = () => {
             callbackURL: process.env.BETTER_AUTH_URL,
         });
         if (res) {
-            toast.success("Login Up Successful");
+            toast.success("Login Successful");
+            router.push('/')
+        }
+
+        if (error) {
+            toast.error(error.message)
+        }
+
+    }
+    const handleGoogleLogin = async () => {
+        const { data: res, error } = await authClient.signIn.social({
+            provider: "google",
+        });
+        if (res) {
+            toast.success("Login Successful");
             router.push('/')
         }
 
@@ -33,34 +47,39 @@ const LoginPage = () => {
 
     return (
         <div className='h-screen flex justify-center items-center mx-auto'>
-            <form onSubmit={handleSubmit(handleLogin)} className='bg-white rounded-lg shadow-2xl p-6 sm:w-sm space-y-2'>
+            <div className='bg-white rounded-lg shadow-2xl p-6 sm:w-sm '>
                 <h2 className='text-3xl text-center'>Please Login!</h2>
                 {/* Google */}
-                <button className="btn bg-white text-black my-5 w-full border-[#e5e5e5]">
+                <button
+                    onClick={handleGoogleLogin}
+                    className="btn bg-white text-black my-5 w-full border-[#e5e5e5]">
                     <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
                     Login with Google
                 </button>
+                <form onSubmit={handleSubmit(handleLogin)} className='space-y-2'>
 
-                <fieldset className="fieldset">
-                    <label className="label">Email</label>
-                    <input
-                        type="email"
-                        className="input w-full"
-                        placeholder="Email"
-                        {...register("email", { required: "Email is Required" })} />
 
-                </fieldset>
-                <fieldset className="fieldset">
-                    <label className="label">Password</label>
-                    <input
-                        type="password"
-                        className="input w-full"
-                        placeholder="Password"
-                        {...register("password", { required: "Password is required" })} />
-                </fieldset>
-                <button className="btn btn-neutral mt-4 w-full">Login</button>
-                <h2>Don't have an account? <Link href={'/auth/register'} className='text-blue-800'>Register</Link></h2>
-            </form>
+                    <fieldset className="fieldset">
+                        <label className="label">Email</label>
+                        <input
+                            type="email"
+                            className="input w-full"
+                            placeholder="Email"
+                            {...register("email", { required: "Email is Required" })} />
+
+                    </fieldset>
+                    <fieldset className="fieldset">
+                        <label className="label">Password</label>
+                        <input
+                            type="password"
+                            className="input w-full"
+                            placeholder="Password"
+                            {...register("password", { required: "Password is required" })} />
+                    </fieldset>
+                    <button className="btn btn-neutral mt-4 w-full">Login</button>
+                    <h2>Don't have an account? <Link href={'/auth/register'} className='text-blue-800'>Register</Link></h2>
+                </form>
+            </div>
         </div>
     );
 };
--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -13,10 +13,10 @@ const SignUpPage = () => {
 
     const handleRegister = async (formData) => {
         // console.log(formData)
-        const {name, email, photo, password} = formData;
+        const { name, email, photo, password } = formData;
         // console.log(name, email, photo, password)
 
-        const { data:res, error } = await authClient.signUp.email({
+        const { data: res, error } = await authClient.signUp.email({
             name: name, // required
             email: email, // required
             password: password, // required
@@ -24,64 +24,81 @@ const SignUpPage = () => {
             callbackURL: process.env.BETTER_AUTH_URL,
         });
 
-        if(error){
+        if (error) {
             toast.error(error.message)
         }
-        if(res){
+        if (res) {
             toast.success("Sign Up Successful")
             router.push('/')
         }
     }
+    const handleGoogleLogin = async () => {
+        const { data: res, error } = await authClient.signIn.social({
+            provider: "google",
+        });
+        if (res) {
+            toast.success("Login Successful");
+            router.push('/')
+        }
+
+        if (error) {
+            toast.error(error.message)
+        }
+
+    }
 
     return (
         <div className='h-screen flex justify-center items-center mx-auto'>
-            <form onSubmit={handleSubmit(handleRegister)} className='bg-white rounded-lg shadow-2xl p-6 sm:w-sm space-y-2'>
+            <div className='bg-white rounded-lg shadow-2xl p-6 sm:w-sm '>
                 <h2 className='text-3xl text-center'>Please Sign Up!</h2>
                 {/* Google */}
-                <button className="btn bg-white text-black my-5 w-full border-[#e5e5e5]">
+                <button onClick={handleGoogleLogin} className="btn bg-white text-black my-5 w-full border-[#e5e5e5]">
                     <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
                     Sign Up with Google
                 </button>
-                <fieldset className="fieldset">
-                    <label className="label">Name</label>
-                    <input
-                        type="text"
-                        className="input w-full"
-                        placeholder="Name"
-                        {...register("name", { required: "Name is required" })}
-                    />
-                    {errors.name && <small className='text-red-500'>{errors.name.message}</small>}
+                <form onSubmit={handleSubmit(handleRegister)} className='space-y-2'>
 
-                </fieldset>
-                <fieldset className="fieldset">
-                    <label className="label">Email</label>
-                    <input
-                        type="email"
-                        className="input w-full"
-                        placeholder="Email"
-                        {...register("email", { required: "Email is Required" })} />
+                    <fieldset className="fieldset">
+                        <label className="label">Name</label>
+                        <input
+                            type="text"
+                            className="input w-full"
+                            placeholder="Name"
+                            {...register("name", { required: "Name is required" })}
+                        />
+                        {errors.name && <small className='text-red-500'>{errors.name.message}</small>}
 
-                </fieldset>
-                <fieldset className="fieldset">
-                    <label className="label">Photo URL</label>
-                    <input
-                        type="text"
-                        className="input w-full"
-                        placeholder="photo"
-                        {...register("photo", { required: "Photo is Required" })} />
+                    </fieldset>
+                    <fieldset className="fieldset">
+                        <label className="label">Email</label>
+                        <input
+                            type="email"
+                            className="input w-full"
+                            placeholder="Email"
+                            {...register("email", { required: "Email is Required" })} />
 
-                </fieldset>
-                <fieldset className="fieldset">
-                    <label className="label">Password</label>
-                    <input
-                        type="password"
-                        className="input w-full"
-                        placeholder="Password"
-                        {...register("password", { required: "Password is required" })} />
-                </fieldset>
-                <button className="btn btn-neutral mt-4 w-full">Sign Up</button>
-                <h2>Already have an account? <Link href={'/auth/login'} className='text-blue-800'>Login</Link></h2>
-            </form>
+                    </fieldset>
+                    <fieldset className="fieldset">
+                        <label className="label">Photo URL</label>
+                        <input
+                            type="text"
+                            className="input w-full"
+                            placeholder="photo"
+                            {...register("photo", { required: "Photo is Required" })} />
+
+                    </fieldset>
+                    <fieldset className="fieldset">
+                        <label className="label">Password</label>
+                        <input
+                            type="password"
+                            className="input w-full"
+                            placeholder="Password"
+                            {...register("password", { required: "Password is required" })} />
+                    </fieldset>
+                    <button className="btn btn-neutral mt-4 w-full">Sign Up</button>
+                    <h2>Already have an account? <Link href={'/auth/login'} className='text-blue-800'>Login</Link></h2>
+                </form>
+            </div>
         </div>
     );
 };
