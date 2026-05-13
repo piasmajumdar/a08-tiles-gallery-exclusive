55f3fe2ea169e0fddc8531a3aed7a745d78f6fa9
piasmajumdar <piasmajumdar@gmail.com>
2026-05-13 12:47:22 +0530

    added not-found page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,8 +1,44 @@
-"use client";
-
+import Link from "next/link";
+import { Home, Search } from "lucide-react";
+import { playFair } from "./layout";
 
 export default function NotFound() {
     return (
-        <>Not Found</>
+        <section className="min-h-screen bg-[#F8F7F4] flex items-center justify-center px-6">
+            <div className="max-w-6xl mx-auto items-center">
+                <div>
+                    <p className="text-[#C8A96B] font-semibold tracking-[4px] uppercase mb-4">
+                        Error 404
+                    </p>
+
+                    <h1
+                        className={`${playFair.className} text-5xl md:text-7xl font-bold leading-tight text-[#1E1E1E]`}
+                    >
+                        Page Not <br /> Found
+                    </h1>
+
+                    <p className="mt-6 text-lg text-gray-600 leading-8 max-w-xl">
+                        The page you are looking for may have been removed,
+                        renamed, or is temporarily unavailable.
+                    </p>
+
+                    <div className="flex flex-wrap gap-4 mt-10">
+                        <Link href="/">
+                            <button className="flex items-center gap-2 cursor-pointer bg-black text-white px-7 py-4 rounded-full text-sm font-medium hover:scale-105 transition-all duration-300">
+                                <Home size={18} />
+                                Back Home
+                            </button>
+                        </Link>
+
+                        <Link href="/all-tiles">
+                            <button className="flex items-center gap-2 border border-gray-300 px-7 py-4 rounded-full text-sm font-medium hover:bg-white hover:scale-105 cursor-pointer transition-all duration-300">
+                                <Search size={18} />
+                                Browse Tiles
+                            </button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
