409eb53afcb67cfdd99128ac7e3d4a1201a78e1c
piasmajumdar <piasmajumdar@gmail.com>
2026-05-14 19:00:12 +0530

    Profile Page Completed

--- src/app/(main)/profile/page.jsx
+++ src/app/(main)/profile/page.jsx
@@ -1,10 +1,95 @@
 
-const MyProfile = () => {
+import { auth } from "@/lib/auth";
+
+import { headers } from "next/headers";
+import Image from "next/image";
+import Link from "next/link";
+
+import { BiCategory } from "react-icons/bi";
+import { FaRegSquare, FaVectorSquare, FaRegCopy, FaUser } from "react-icons/fa";
+import { MdAddAPhoto, MdEmail, MdOutlineManageAccounts } from "react-icons/md";
+import { RiColorFilterAiLine } from "react-icons/ri";
+
+
+const MyProfilePage = async () => {
+
+    const { user } = await auth.api.getSession({
+        headers: await headers()
+    })
+    console.log(user)
+
     return (
-        <div>
-            MyProfile
-        </div>
+        <div className="bg-[#f8f7f4] min-h-screen p-4 md:p-8 mt-20">
+            <div className="max-w-lg mx-auto bg-white rounded-[30px] p-5 md:p-8 shadow-sm">
+
+                <div className="breadcrumbs text-sm text-gray-400 mb-6">
+                    <ul>
+                        <li>Home</li>
+                        <li className="text-black">My Profile</li>
+                    </ul>
+                </div>
+
+
+                <div>
+
+                    <div>
+                        <div className="flex gap-6 flex-wrap">
+                            <figure className=' w-[100px] h-[100px] rounded-full overflow-hidden flex justify-center items-center'>
+                                <Image
+                                    src={user?.image}
+                                    alt={user?.name}
+                                    height={400}
+                                    width={400}
+                                    className='h-full w-full' />
+                            </figure>
+                            <div>
+                                <h1 className="text-xl font-bold text-black">{user?.name}</h1>
+                                <p className="font-semibold mt-4 text-black">{user?.email}</p>
+                                <div className="mt-4">
+                                    <div className="badge badge-success text-white px-4 py-3">
+                                        Member
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="mt-8 space-y-5">
+                            
+                            <div className="flex items-center justify-between border-b border-gray-100 pb-3">
+                                <div className="flex items-center gap-3">
+                                    <MdOutlineManageAccounts className="text-[#b08244] text-2xl" />
+                                    <span className="font-semibold">Name</span>
+                                </div>
+                                <span className="text-gray-600">{user?.name}</span>
+                            </div>
+                            
+                            <div className="flex items-center justify-between border-b border-gray-100 pb-3">
+                                <div className="flex items-center gap-3">
+                                    <MdEmail className="text-[#b08244] text-2xl" />
+                                    <span className="font-semibold">Email</span>
+                                </div>
+                                <span className="text-gray-600">{user?.email}</span>
+                            </div>
+
+                            <div className="flex items-center justify-between border-b border-gray-100 pb-3">
+                                <div className="flex items-center gap-3">
+                                    <MdAddAPhoto className="text-[#b08244] text-4xl" />
+                                    <span className="font-semibold">Photo URL</span>
+                                </div>
+                                <span className="text-gray-600">{user?.image}</span>
+                            </div>
+
+                        </div>
+
+                        <Link href={'/profile/update'} className="btn w-full text-white bg-black rounded-lg mt-6"><FaUser /> Update Profile</Link>
+
+
+                    </div>
+                </div>
+            </div>
+        </div >
     );
 };
 
-export default MyProfile;
+export default MyProfilePage;
