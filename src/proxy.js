a5514f385a5b43f3c1b89dc82df22eb43fd44b85
piasmajumdar <piasmajumdar@gmail.com>
2026-05-14 18:18:28 +0530

    proxy set up completed for profile and tile-details page

---
+++ src/proxy.js
@@ -0,0 +1,24 @@
+import { NextResponse } from 'next/server'
+import { auth } from './lib/auth'
+import { headers } from 'next/headers'
+
+// This function can be marked `async` if using `await` inside
+export async function proxy(request) {
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+    // console.log(session)
+
+    if (session) {
+        return NextResponse.next();
+    } else {
+        return NextResponse.redirect(new URL('/auth/login', request.url))
+    }
+}
+
+// Alternatively, you can use a default export:
+// export default function proxy(request) { ... }
+
+export const config = {
+    matcher: ['/profile', '/tile-details/:path*'],
+}
