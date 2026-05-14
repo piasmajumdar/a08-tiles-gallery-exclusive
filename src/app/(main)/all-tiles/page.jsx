21da02143a71a4fdbe71a6563f8cd843ce262d3c
piasmajumdar <piasmajumdar@gmail.com>
2026-05-14 17:10:32 +0530

    Search Bar and Search Functionality Implemented

--- src/app/(main)/all-tiles/page.jsx
+++ src/app/(main)/all-tiles/page.jsx
@@ -1,19 +1,42 @@
 import { playFair } from "@/app/layout";
+import SearchBar from "@/components/shared/SearchBar/SearchBar";
 import TilesCard from "@/components/shared/TilesCard/TilesCard";
 import { getAllTiles } from "@/lib/data";
 import Link from "next/link";
 
-const AllTilesPage = async() => {
+const AllTilesPage = async ({ searchParams }) => {
+    const { search } = await searchParams;
+    // console.log(search);
+
     const tiles = await getAllTiles();
     // console.log(tiles)
+    let filtered_tiles = [...tiles]
+
+    if (search) {
+        const searchText = search.toLowerCase().trim();
+
+        filtered_tiles = tiles.filter(tile =>
+            tile.title?.toLowerCase().includes(searchText) ||
+            tile.category?.toLowerCase().includes(searchText) ||
+            tile.description?.toLowerCase().includes(searchText) ||
+            tile.material?.toLowerCase().includes(searchText)
+        );
+
+    } else {
+
+    }
+    // console.log(filtered_tiles);
+
+
     return (
         <div className="max-w-11/12 mx-auto py-6 mt-20">
             <div className="my-2">
-                <h2 className={`text-3xl font-bold ${playFair.className}`}>All Tiles</h2>
+                {/* <h2 className={`text-3xl font-bold ${playFair.className}`}>All Tiles</h2> */}
+                <SearchBar></SearchBar>
             </div>
             <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
                 {
-                    tiles.map(tile => <TilesCard key={tile.id} tile={tile}></TilesCard>)
+                    filtered_tiles.map(tile => <TilesCard key={tile.id} tile={tile}></TilesCard>)
                 }
             </div>
         </div>
---
+++ src/components/shared/SearchBar/SearchBar.jsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+import { IoSearch } from "react-icons/io5";
+
+export default function SearchBar() {
+
+    const [searchInp, setSearchInp] = useState("");
+    const searchParams = useSearchParams();
+    const router = useRouter();
+    const pathName = usePathname();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        const params = new URLSearchParams(searchParams);
+        searchInp? params.set('search', searchInp) : params.delete('search');
+        router.push(`${pathName}?${params.toString()}`)
+
+    }
+
+    return (
+        <section className="w-full bg-[#f8f7f4] pb-4 px-4">
+            <div className="max-w-5xl mx-auto">
+
+                {/* Heading */}
+                <div className="text-center mb-8">
+                    <h1 className={`text-5xl font-bold text-[#1e1e1e]`}>
+                        Discover Premium Tiles
+                    </h1>
+
+                    <p className="text-gray-500 mt-4 text-lg">
+                        Search tiles by title, material, style, or aesthetic.
+                    </p>
+                </div>
+
+                {/* Search Box */}
+                <form onSubmit={handleSubmit} className="relative">
+                    <input
+                        defaultValue={''}
+                        onChange={(e)=>setSearchInp(e.target.value)}
+                        type="text"
+                        name="searchInput"
+                        placeholder="Search tiles by title..."
+                        className="w-full h-[72px] rounded-full bg-white pl-8 pr-36 text-lg text-black placeholder:text-gray-400 border border-gray-200 outline-none shadow-[0_10px_40px_rgba(0,0,0,0.06)] focus:border-black transition-all duration-300"
+                    />
+
+                    {/* Search Button */}
+                    <button className="absolute right-3 top-1/2 -translate-y-1/2 h-[58px] px-8 rounded-full bg-black hover:bg-[#b08244] text-white text-base font-semibold flex items-center gap-2 transition-all duration-300">
+                        <IoSearch size={22} />
+                        Search
+                    </button>
+                </form>
+            </div>
+        </section>
+    );
+}
